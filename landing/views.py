464e800cdc3d227e13107e3d60eb7a1df6f75607
TMats <t.matsu.changmin@gmail.com>
2018-07-10 13:56:54 +0900

    modify landing page

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from release.models import Releases
+
+TRACKS_LIMIT = 20
+
 
 def index(request):
+    releases = Releases.objects.filter(enabled=True).order_by('updated_at').reverse()[:TRACKS_LIMIT]
     context = {
+        'releases': releases
     }
     template = loader.get_template('landing/index.html')
     return HttpResponse(template.render(context, request))
